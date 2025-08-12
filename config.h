e3871af71160a6486b3c265e204692154614ca79
Yuu Murasaki <hoangthibaongoc411@gmail.com>
2025-08-12 21:47:06 +0700

    fix

--- config.h
+++ config.h
@@ -11,8 +11,8 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#cdd6f4", "#1e1e2e" },
-	[SchemeSel] = { "#1e1e2e", "#89dceb" },
-	[SchemeOut] = { "#000000", "#89dceb" },
+	[SchemeSel] = { "#1e1e2e", "#cba6f7" },
+	[SchemeOut] = { "#000000", "#cba6f7" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
